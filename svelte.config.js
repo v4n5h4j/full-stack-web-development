aa3c432c3d1393983c8bd0b21ad762a4b77f97ab
v4n5h4j <vanshaj82@gmail.com>
2022-01-10 18:06:45 +0000

    Improve comments
    Improve a few comments in gitpod.yml and svelte.config file
    - 2 files contains elaborated commit messages
    - coming back after a couple of weeks
    - I hope I will be able to continue from here

--- svelte.config.js
+++ svelte.config.js
@@ -22,6 +22,7 @@ const config = {
 					clientPort: process.env.HMR_HOST ? 443 : 3000,
 					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost" 
 					// Now, we can run the dev env. both locally and on Gitpod and host is always set correctly.
+					// We get the benefit of HMR both on Gitpod and local environment
 					// HMR_HOST exits only when Gitpod (see '.gitpod.yml')
 					// HMR_HOST is including https as protocol, vit doesn't like that. It just hosts and use websockets.
 				}
